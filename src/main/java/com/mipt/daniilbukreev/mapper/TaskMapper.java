77477aace2150b74a634d71f100b08e76c7046ef
daniilbukreev <daniilbukreev>
2026-04-07 15:11:25 +0300

    mapper changes

--- src/main/java/com/mipt/daniilbukreev/mapper/TaskMapper.java
+++ src/main/java/com/mipt/daniilbukreev/mapper/TaskMapper.java
@@ -5,6 +5,7 @@ import com.mipt.daniilbukreev.dto.TaskResponseDto;
 import com.mipt.daniilbukreev.dto.TaskUpdateDto;
 import com.mipt.daniilbukreev.model.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
@@ -12,12 +13,25 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        uses = {AttachmentMapper.class}
 )
 public interface TaskMapper {
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "attachments", ignore = true)
     Task toEntity(TaskCreateDto dto);
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "attachments", ignore = true)
     Task updateEntity(TaskUpdateDto dto, @MappingTarget Task task);
 
+    @Mapping(target = "tags", expression = "java(task.getTags())")
+    @Mapping(target = "dueDate", source = "dueDate")
+    @Mapping(target = "attachments", source = "attachments")
     TaskResponseDto toResponseDto(Task task);
 }
